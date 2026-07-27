1d2ad8428352a9e051418f6db15812b7ef7f829d
pranayh916 <pranayh916@gmail.com>
2026-07-27 14:56:40 +0530

    Add files via upload

---
+++ Assignment/_22_july_assignment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _22_july_assignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
