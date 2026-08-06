42a16b10f1f106018bc3fa5efe9a3c16ebf4923d
pranayh916 <pranayh916@gmail.com>
2026-08-06 08:21:44 +0530

    Add files via upload

---
+++ Assignment/28th_july_Assignment/Controllers/RegisterController.cs
@@ -0,0 +1,50 @@
+﻿using _28_July_Assignment.Models;
+using _28_July_Assignment.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _28_July_Assignment.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RegisterController : ControllerBase
+    {
+        private readonly IRegister _service;
+
+        public RegisterController(IRegister service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_service.DisplayCourse());
+        }
+
+        [HttpPost]
+        public IActionResult RegisterCourse(Course course)
+        {
+            return Ok(_service.RegisterCourse(course));
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateCourse(int id, Course course)
+        {
+            var result = _service.UpdateCourse(id, course);
+
+            if (result == null)
+                return NotFound("Course Not Found");
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCourse(int id)
+        {
+            if (_service.CancelCourse(id))
+                return Ok("Course Deleted Successfully");
+
+            return NotFound("Course Not Found");
+        }
+    }
+}
---
+++ Assignment/28th_july_Assignment/Services/IRegister.cs
@@ -0,0 +1,15 @@
+﻿using _28_July_Assignment.Models;
+
+namespace _28_July_Assignment.Services
+{
+    public interface IRegister
+    {
+        List<Course> DisplayCourse();
+
+        Course RegisterCourse(Course course);
+
+        Course? UpdateCourse(int id, Course course);
+
+        bool CancelCourse(int id);
+    }
+}
