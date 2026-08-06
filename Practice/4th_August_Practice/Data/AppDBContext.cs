19c00b7467a89c8827c6f78e8faa510526c759d3
pranayh916 <pranayh916@gmail.com>
2026-08-06 08:17:08 +0530

    Add files via upload

---
+++ Practice/4th_August_Practice/Data/AppDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using _4_august_practice.Models;
+
+namespace _4_august_practice.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ Practice/4th_August_Practice/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _4_august_practice.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public int Age { get; set; }
+
+        [Required]
+        public string Course { get; set; } = string.Empty;
+
+        [Required]
+        public string City { get; set; } = string.Empty;
+    }
+}
